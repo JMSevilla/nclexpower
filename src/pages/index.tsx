3012940ba6eb636e331dc6dc7532e0602ca8036c
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2024-03-24 22:34:58 +0800

    @test/5418873/code-ref-fe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,30 +2,26 @@ import { Header, Footer, QContainer, CSQContainer, Card } from "@/components";
 import React, { useEffect, useState } from "react";
 import { GetServerSideProps } from "next";
 import { InitA, InitQ } from "@/core/ssr/api";
-
+import { SsrData } from "@/core/types";
 
 interface userType {
-  name: string
-  age: number
+  name: string;
+  age: number;
 }
 
 interface Props {
   data?: any;
   error?: any;
-  user: {
-
-  }
-
+  user: {};
 }
 
 export default function Home({ data, error }: Props) {
-
+  console.log(data.prefetchQ);
   return (
-    <div className='h-[71dvh] '>
+    <div className="h-[71dvh] ">
       <Header />
-      <QContainer />
+      <QContainer questionaire={data.prefetchQ as SsrData["questionaire"]} />
       <Footer />
-
     </div>
   );
 }
